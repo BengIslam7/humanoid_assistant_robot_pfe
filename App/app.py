f245d99f0d5e080be737f34c01c5421a5085d9d1
BengIslam7 <devmlpentBMI66@gmail.com>
2025-05-26 15:21:05 +0100

    Update ofor the app

--- App/app.py
+++ App/app.py
@@ -31,6 +31,72 @@ def Home(event):
     x=client.publish("dest", payload="H", qos=1)
     print(x)
 
+def forwardl(event):
+    value = linear_vel.get()
+    value2 = angular_vel.get()
+    cmd="u "+value+" "+value2
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def forward(event):
+    value = linear_vel.get()
+    cmd="i "+value
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def forwardr(event):
+    value = linear_vel.get()
+    value2 = angular_vel.get()
+    cmd="o "+value+" "+value2
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def stop(event):
+    cmd="k"
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def left(event):
+    value2 = angular_vel.get()
+    cmd="j "+value2
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def right(event):
+    value2 = angular_vel.get()
+    cmd="l "+value2
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def backwardr(event):
+    value = linear_vel.get()
+    value2 = angular_vel.get()
+    cmd=". "+value+" "+value2
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def backwardl(event):
+    value = linear_vel.get()
+    value2 = angular_vel.get()
+    cmd="m "+value+" "+value2
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+    
+def backward(event):
+    value = linear_vel.get()
+    cmd=", "+value
+    print(cmd)
+    x=client.publish("cmd", payload=cmd, qos=1)
+    print(x)
+
 root = tk.Tk()
 root.title(title)
 root.geometry("400x550")
@@ -46,17 +112,64 @@ poslabel = tk.Label(root, width=27, text="Robot Position = Home",font=("Arial", 
 poslabel.pack(pady=5)
 button = tk.Button(frame, text="Home")
 button2 = tk.Button(frame, text="A")
-button3 = tk.Button(root, text="B")
+button3 = tk.Button(frame, text="B")
 button.bind("<Button-1>", Home)
 button2.bind("<Button-1>", A)
 button3.bind("<Button-1>", B)
 dest = tk.Label(root, width=27, text="Destination",font=("Arial", 15),foreground="blue")
 dest.pack(pady=5)
 button.pack(side=tk.LEFT, padx=10)
-button2.pack(side=tk.LEFT, padx=5)
+button2.pack(side=tk.LEFT, padx=10)
 button3.pack(pady=10)
-#button.pack(pady=5)
-#button2.pack(pady=10)
-#button3.pack(pady=15)
+#control
+line1 = tk.Frame(root)
+line2 = tk.Frame(root)
+line3 = tk.Frame(root)
+ctr = tk.Label(line1, width=27, text="Controle",font=("Arial", 15),foreground="blue")
+ctr.pack(pady=5)
+linear_vel = tk.Entry(line1, width=10)
+angular_vel = tk.Entry(line1, width=10)
+linear_vel.pack(padx=10, pady=10)
+angular_vel.pack(padx=10, pady=10)
+# Define buttons
+fwdl = tk.Button(line1, text="↖")
+fwd = tk.Button(line1, text="↑")
+fwdr = tk.Button(line1, text="↗")
+bleft = tk.Button(line2, text="←")
+bstop = tk.Button(line2, text="■")
+bright = tk.Button(line2, text="→")
+bwdl = tk.Button(line3, text="↙")
+bwd = tk.Button(line3, text="↓")
+bwdr = tk.Button(line3, text="↘")
+fwdl.pack(side=tk.LEFT, padx=40,pady=10)
+fwdl.bind("<Button-1>", forwardl)
+
+fwd.pack(side=tk.LEFT, padx=40,pady=10)
+fwd.bind("<Button-1>", forward)
+
+fwdr.pack(side=tk.LEFT, padx=40,pady=10)
+fwdr.bind("<Button-1>", forwardr)
+
+bleft.pack(side=tk.LEFT, padx=40)
+bleft.bind("<Button-1>", left)
+
+bstop.pack(side=tk.LEFT, padx=40)
+bstop.bind("<Button-1>", stop)
+
+bright.pack(side=tk.LEFT, padx=40)
+bright.bind("<Button-1>", right)
+
+bwdl.pack(side=tk.LEFT, padx=40,pady=10)
+bwdl.bind("<Button-1>", backwardl)
+
+bwd.pack(side=tk.LEFT, padx=40,pady=10)
+bwd.bind("<Button-1>", backward)
+
+bwdr.pack(side=tk.LEFT, padx=40,pady=10)
+bwdr.bind("<Button-1>", backwardr)
+
 frame.pack()
+line1.pack()
+line2.pack()
+line3.pack()
 root.mainloop()
--- App/raspcode.py
+++ App/raspcode.py
@@ -6,24 +6,84 @@ client = paho.Client()
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 client.username_pw_set("hivemq.webclient.1745834546662", "Fge.w;f!d36OW#4T9AYn")
 client.connect("3a06204e5f944e08b8d312754d3f8ec7.s1.eu.hivemq.cloud", 8883)
+cmd1 = 'ros2 topic pub /diff_cont/cmd_vel_unstamped geometry_msgs/msg/Twist "{linear: {x: '
+cmd2 = ', y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: '
+cmd3 = '}}" --once'
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    mp=msg.payload.decode('utf-8')
+    print(mp)
     if(str(msg.payload)=="b'A'"):
-    	client.publish("pos", payload="A", qos=1)
-        print("A Destination")
+        client.publish("pos", payload="A", qos=1)
     elif(str(msg.payload)=="b'B'"):
     	client.publish("pos", payload="B", qos=1)
-        print("B Destination")
     elif(str(msg.payload)=="b'H'"):
+    	print(str(msg.payload)[2])
     	client.publish("pos", payload="H", qos=1)
-        print("Home Destination")
+    elif(mp[0]=="i"):
+    	linear=mp.split(' ')[1]
+    	angular='0.0'
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="u"):
+    	linear=mp.split(' ')[1]
+    	angular=mp.split(' ')[2]
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="o"):
+    	linear=mp.split(' ')[1]
+    	angular=mp.split(' ')[2]
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="k"):
+    	linear='0.0'
+    	angular='0.0'
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="j"):
+    	linear='0.0'
+    	angular=mp.split(' ')[1]
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="l"):
+    	linear='0.0'
+    	angular=mp.split(' ')[1]
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="m"):
+    	linear=mp.split(' ')[1]
+    	angular=mp.split(' ')[2]
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]=="."):
+    	linear=mp.split(' ')[1]
+    	angular=mp.split(' ')[2]
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    elif(mp[0]==","):
+    	linear=mp.split(' ')[1]
+    	angular='0.0'
+    	pub=cmd1+linear+cmd2+angular+cmd3
+    	print(pub)
+    	os.system(pub)
+    
+    	
 
 client.on_subscribe = on_subscribe
 client.on_message = on_message
 client.subscribe('dest', qos=1)
+client.subscribe('cmd', qos=1)
 client.loop_forever()
 
